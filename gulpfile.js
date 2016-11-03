38fa954776718d608cd297613bf934e23c226735
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-03 14:22:46 +0100

    Add Gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,44 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var tsify = require("tsify");
+var watchify = require("watchify");
 var paths = {
     pages: ['src/*.html']
 };
+
+var browserifyInstance = browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+})
+    .plugin(tsify);
 
 gulp.task("copy-html", function () {
     return gulp.src(paths.pages)
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", ["copy-html"], function () {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
+gulp.task("browserify", function () {
+    return browserifyInstance
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["copy-html", "browserify"]);
+
+gulp.task("watch", function () {
+    // Watch for changed html files
+    gulp.watch('src/*.html', ['copy-html']);
+
+    // Watch files for changes through Browserify with Watchify
+    return browserifyInstance
+        .plugin(watchify)
+        // When a file has changed, rerun the browserify task to create an updated bundle
+        .on('update', function () {
+            gulp.start("browserify");
+        })
+        .bundle();
 });
