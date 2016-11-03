15d12e5324ba75d089cab101c4a458bd108ce8e0
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-03 14:39:54 +0100

    Configure all Gulp paths centrally

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,55 @@ var source = require('vinyl-source-stream');
 var tsify = require("tsify");
 var watchify = require("watchify");
 var sass = require('gulp-sass');
+
 var paths = {
-    pages: ['src/*.html']
+    source: {
+        html: ['./src/*.html'],
+        ts: ['./src/main.ts'],
+        sass: ['./src/*.scss']
+    },
+    target: {
+        html: './dist',
+        js: './dist',
+        css: './dist'
+    }
 };
 
 var browserifyInstance = browserify({
     basedir: '.',
     debug: true,
-    entries: ['src/main.ts'],
+    entries: paths.source.ts,
     cache: {},
     packageCache: {}
 })
     .plugin(tsify);
 
 gulp.task("copy-html", function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
+    return gulp.src(paths.source.html)
+        .pipe(gulp.dest(paths.target.html));
 });
 
 gulp.task("browserify", function () {
     return browserifyInstance
         .bundle()
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest("dist"));
+        .pipe(gulp.dest(paths.target.js));
 });
 
-gulp.task('sass', function() {
-    gulp.src('./src/*.scss')
+gulp.task('sass', function () {
+    gulp.src(paths.source.sass)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest(paths.target.css));
 });
 
 gulp.task("default", ["copy-html", "browserify", "sass"]);
 
 gulp.task("watch", function () {
     // Watch for changed html files
-    gulp.watch('src/*.html', ['copy-html']);
+    gulp.watch(paths.source.html, ['copy-html']);
 
     // Watch SASS files
-    gulp.watch('./src/*.scss', ['sass']);
+    gulp.watch(paths.source.sass, ['sass']);
 
     // Watch files for changes through Browserify with Watchify
     return browserifyInstance
