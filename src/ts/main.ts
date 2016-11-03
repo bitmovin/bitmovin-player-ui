09e63ef0e282557f550a13c81f38925286c96705
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-03 15:32:06 +0100

    Remove ts sample code

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,3 @@
-import {sayHello} from "./greet";
 import {Button} from "./button";
-
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = sayHello(name);
-}
-
-showHello("greeting", "TypeScript");
 
 var test2 = new Button({ id: 'testid', text: 'blubb' });
