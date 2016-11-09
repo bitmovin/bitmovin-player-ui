b5ef4ecdcb8a5823e414b360ed7403b0b036adcf
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-09 16:07:19 +0100

    Add item add/remove/select events to listselector

--- src/ts/components/listselector.ts
+++ src/ts/components/listselector.ts
@@ -1,4 +1,5 @@
 import {Component, ComponentConfig} from "./component";
+import {EventDispatcher, Event} from "../eventdispatcher";
 
 export interface ListItemCollection {
     // value -> label mapping
@@ -11,12 +12,91 @@ export interface ListSelectorConfig extends ComponentConfig {
 
 export abstract class ListSelector<Config extends ListSelectorConfig> extends Component<ListSelectorConfig> {
 
-    constructor(config: ListSelectorConfig) {
+    protected items: ListItemCollection;
+    protected selectedItem: string;
+
+    protected listSelectorEvents = {
+        onItemAdded: new EventDispatcher<ListSelector<Config>, string>(),
+        onItemRemoved: new EventDispatcher<ListSelector<Config>, string>(),
+        onItemSelected: new EventDispatcher<ListSelector<Config>, string>()
+    };
+
+    constructor(config: ListSelectorConfig = {}) {
         super(config);
 
         this.config = this.mergeConfig(config, {
             items: {},
             cssClass: 'ui-listselector'
         });
+
+        this.items = this.config.items;
+    }
+
+    hasItem(value: string): boolean {
+        return this.items[value] != null;
+    }
+
+    addItem(value: string, label: string) {
+        this.items[value] = label;
+        this.onItemAddedEvent(value);
+    }
+
+    removeItem(value: string): boolean {
+        if(this.hasItem(value)) {
+            delete this.items[value];
+            this.onItemRemovedEvent(value);
+            return true;
+        }
+
+        return false;
+    }
+
+    selectItem(value: string): boolean {
+        if(value == this.selectedItem) {
+            // item is already selected, suppress any further action
+            return true;
+        }
+
+        if(this.items[value] != null) {
+            this.selectedItem = value;
+            this.onItemSelectedEvent(value);
+            return true;
+        }
+
+        return false;
+    }
+
+    clearItems() {
+        let items = this.items; // local copy for iteration after clear
+        this.items = {}; // clear items
+
+        // fire events
+        for(let value in items) {
+            this.onItemRemovedEvent(value);
+        }
+    }
+
+    protected onItemAddedEvent(value: string) {
+        this.listSelectorEvents.onItemAdded.dispatch(this, value);
+    }
+
+    protected onItemRemovedEvent(value: string) {
+        this.listSelectorEvents.onItemRemoved.dispatch(this, value);
+    }
+
+    protected onItemSelectedEvent(value: string) {
+        this.listSelectorEvents.onItemSelected.dispatch(this, value);
+    }
+
+    get onItemAdded(): Event<ListSelector<Config>, string> {
+        return this.listSelectorEvents.onItemAdded;
+    }
+
+    get onItemRemoved(): Event<ListSelector<Config>, string> {
+        return this.listSelectorEvents.onItemRemoved;
+    }
+
+    get onItemSelected(): Event<ListSelector<Config>, string> {
+        return this.listSelectorEvents.onItemSelected;
     }
 }
