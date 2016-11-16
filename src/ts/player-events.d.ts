a0adf92ff2282a708e8f5ff05fa925e8b8b2f1b6
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-16 10:35:52 +0100

    Move player event definitions to separate file
    Definition file would get too huge once all events are defined.

---
+++ src/ts/player-events.d.ts
@@ -0,0 +1,91 @@
+declare namespace bitmovin {
+
+    namespace player {
+
+        enum EVENT {
+            ON_AD_CLICKED,
+            ON_AD_ERROR,
+            ON_AD_FINISHED,
+            ON_AD_LINEARITY_CHANGED,
+            ON_AD_MANIFEST_LOADED,
+            ON_AD_SCHEDULED,
+            ON_AD_SKIPPED,
+            ON_AD_STARTED,
+            ON_AUDIO_ADAPTATION,
+            ON_AUDIO_CHANGE,
+            ON_AUDIO_DOWNLOAD_QUALITY_CHANGE,
+            ON_AUDIO_PLAYBACK_QUALITY_CHANGE,
+            ON_CAST_AVAILABLE,
+            ON_CAST_LAUNCHED,
+            ON_CAST_PAUSE,
+            ON_CAST_PLAYBACK_FINISHED,
+            ON_CAST_PLAYING,
+            ON_CAST_START,
+            ON_CAST_STOP,
+            ON_CAST_TIME_UPDATE,
+            ON_CAST_WAITING_FOR_DEVICE,
+            ON_CUE_ENTER,
+            ON_CUE_EXIT,
+            ON_DOWNLOAD_FINISHED,
+            ON_DVR_WINDOW_EXCEEDED,
+            ON_ERROR,
+            ON_FULLSCREEN_ENTER,
+            ON_FULLSCREEN_EXIT,
+            ON_HIDE_CONTROLS,
+            ON_METADATA,
+            ON_MUTE,
+            ON_PAUSE,
+            ON_PERIOD_SWITCHED,
+            ON_PLAY,
+            ON_PLAYBACK_FINISHED,
+            ON_PLAYER_RESIZE,
+            ON_READY,
+            ON_SEEK,
+            ON_SEEKED,
+            ON_SEGMENT_REQUEST_FINISHED,
+            ON_SHOW_CONTROLS,
+            ON_SOURCE_LOADED,
+            ON_SOURCE_UNLOADED,
+            ON_START_BUFFERING,
+            ON_STOP_BUFFERING,
+            ON_SUBTITLE_ADDED,
+            ON_SUBTITLE_CHANGE,
+            ON_SUBTITLE_REMOVED,
+            ON_TIME_CHANGED,
+            ON_TIME_SHIFT,
+            ON_TIME_SHIFTED,
+            ON_UNMUTE,
+            ON_VIDEO_ADAPTATION,
+            ON_VIDEO_DOWNLOAD_QUALITY_CHANGE,
+            ON_VIDEO_PLAYBACK_QUALITY_CHANGE,
+            ON_VOLUME_CHANGE,
+            ON_VR_ERROR,
+            ON_VR_MODE_CHANGED,
+            ON_VR_STEREO_CHANGED,
+            ON_WARNING
+        }
+
+        interface PlayerEvent {
+            timestamp: number;
+            type: EVENT;
+        }
+
+        interface SubtitleChangedEvent extends PlayerEvent {
+            time: number;
+            sourceSubtitle: Subtitle;
+            targetSubtitle: Subtitle;
+        }
+
+        interface SubtitleAddedEvent extends PlayerEvent {
+            subtitle: Subtitle;
+        }
+
+        interface SubtitleRemovedEvent extends PlayerEvent {
+            subtitleId: string;
+        }
+
+        interface PlayerEventCallback {
+            (event: PlayerEvent): void;
+        }
+    }
+}
--- src/ts/player.d.ts
+++ src/ts/player.d.ts
@@ -1,31 +1,10 @@
+/// <reference path="player-events.d.ts" />
+
 declare namespace bitmovin {
 
     function player(htmlElementID: string): player.Player;
 
     namespace player {
-
-        interface PlayerEvent {
-            timestamp: number;
-            type: EVENT;
-        }
-
-        interface SubtitleChangedEvent extends PlayerEvent {
-            time: number;
-            sourceSubtitle: Subtitle;
-            targetSubtitle: Subtitle;
-        }
-
-        interface SubtitleAddedEvent extends PlayerEvent {
-            subtitle: Subtitle;
-        }
-
-        interface SubtitleRemovedEvent extends PlayerEvent {
-            subtitleId: string;
-        }
-
-        interface PlayerEventCallback {
-            (event: PlayerEvent): void;
-        }
 
         interface ViewingDirection {
             yaw: number;
@@ -201,69 +180,6 @@ declare namespace bitmovin {
             timeShift(offset: number): Player;
             unload(): Player;
             unmute(): Player;
-        }
-
-        enum EVENT {
-            ON_AD_CLICKED,
-            ON_AD_ERROR,
-            ON_AD_FINISHED,
-            ON_AD_LINEARITY_CHANGED,
-            ON_AD_MANIFEST_LOADED,
-            ON_AD_SCHEDULED,
-            ON_AD_SKIPPED,
-            ON_AD_STARTED,
-            ON_AUDIO_ADAPTATION,
-            ON_AUDIO_CHANGE,
-            ON_AUDIO_DOWNLOAD_QUALITY_CHANGE,
-            ON_AUDIO_PLAYBACK_QUALITY_CHANGE,
-            ON_CAST_AVAILABLE,
-            ON_CAST_LAUNCHED,
-            ON_CAST_PAUSE,
-            ON_CAST_PLAYBACK_FINISHED,
-            ON_CAST_PLAYING,
-            ON_CAST_START,
-            ON_CAST_STOP,
-            ON_CAST_TIME_UPDATE,
-            ON_CAST_WAITING_FOR_DEVICE,
-            ON_CUE_ENTER,
-            ON_CUE_EXIT,
-            ON_DOWNLOAD_FINISHED,
-            ON_DVR_WINDOW_EXCEEDED,
-            ON_ERROR,
-            ON_FULLSCREEN_ENTER,
-            ON_FULLSCREEN_EXIT,
-            ON_HIDE_CONTROLS,
-            ON_METADATA,
-            ON_MUTE,
-            ON_PAUSE,
-            ON_PERIOD_SWITCHED,
-            ON_PLAY,
-            ON_PLAYBACK_FINISHED,
-            ON_PLAYER_RESIZE,
-            ON_READY,
-            ON_SEEK,
-            ON_SEEKED,
-            ON_SEGMENT_REQUEST_FINISHED,
-            ON_SHOW_CONTROLS,
-            ON_SOURCE_LOADED,
-            ON_SOURCE_UNLOADED,
-            ON_START_BUFFERING,
-            ON_STOP_BUFFERING,
-            ON_SUBTITLE_ADDED,
-            ON_SUBTITLE_CHANGE,
-            ON_SUBTITLE_REMOVED,
-            ON_TIME_CHANGED,
-            ON_TIME_SHIFT,
-            ON_TIME_SHIFTED,
-            ON_UNMUTE,
-            ON_VIDEO_ADAPTATION,
-            ON_VIDEO_DOWNLOAD_QUALITY_CHANGE,
-            ON_VIDEO_PLAYBACK_QUALITY_CHANGE,
-            ON_VOLUME_CHANGE,
-            ON_VR_ERROR,
-            ON_VR_MODE_CHANGED,
-            ON_VR_STEREO_CHANGED,
-            ON_WARNING
         }
     }
 }
