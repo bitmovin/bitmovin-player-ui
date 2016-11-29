68e7036614c13c39b6150a174deb3bf4e2fb4d97
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-29 13:17:11 +0100

    Document Event classes

--- src/ts/eventdispatcher.ts
+++ src/ts/eventdispatcher.ts
@@ -20,14 +20,34 @@ export interface EventListener<Sender, Args> {
 export interface NoArgs {
 }
 
+/**
+ * Public interface that represents an event. Can be used to subscribe to and unsubscribe from events.
+ */
 export interface Event<Sender, Args> {
+    /**
+     * Subscribes an event listener to this event dispatcher.
+     * @param listener the listener to add
+     */
     subscribe(listener: EventListener<Sender, Args>): void;
+
+    /**
+     * Subscribes an event listener to this event dispatcher that will be called at a limited rate with a minimum
+     * interval of the specified milliseconds.
+     * @param listener the listener to add
+     * @param rateMs the rate in milliseconds to which calling of the listeners should be limited
+     */
     subscribeRateLimited(listener: EventListener<Sender, Args>, rateMs: number): void;
+
+    /**
+     * Unsubscribes a subscribed event listener from this dispatcher.
+     * @param listener the listener to remove
+     * @returns {boolean} true if the listener was successfully unsubscribed, false if it isn't subscribed on this dispatcher
+     */
     unsubscribe(listener: EventListener<Sender, Args>): boolean;
 }
 
 /**
- * Event dispatcher to subscribe and trigger events. Each event should have it's own dispatcher.
+ * Event dispatcher to subscribe and trigger events. Each event should have its own dispatcher.
  */
 export class EventDispatcher<Sender, Args> implements Event<Sender, Args> {
 
@@ -37,21 +57,21 @@ export class EventDispatcher<Sender, Args> implements Event<Sender, Args> {
     }
 
     /**
-     * Subscribes an event listener to this event dispatcher.
-     * @param listener the listener to add
+     * {@inheritDoc}
      */
     subscribe(listener: EventListener<Sender, Args>) {
         this.listeners.push(new EventListenerWrapper(listener));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     subscribeRateLimited(listener: EventListener<Sender, Args>, rateMs: number) {
         this.listeners.push(new RateLimitedEventListenerWrapper(listener, rateMs));
     }
 
     /**
-     * Unsubscribes a subscribed event listener from this dispatcher.
-     * @param listener the listener to remove
-     * @returns {boolean} true if the listener was successfully unsubscribed, false if it isn't subscribed on this dispatcher
+     * {@inheritDoc}
      */
     unsubscribe(listener: EventListener<Sender, Args>): boolean {
         // Iterate through listeners, compare with parameter, and remove if found
@@ -79,6 +99,10 @@ export class EventDispatcher<Sender, Args> implements Event<Sender, Args> {
     }
 }
 
+/**
+ * A basic event listener wrapper to manage listeners within the {@link EventDispatcher}. This is a "private" class
+ * for internal dispatcher use and it is therefore not exported.
+ */
 class EventListenerWrapper<Sender, Args> {
 
     private eventListener: EventListener<Sender, Args>;
@@ -87,15 +111,27 @@ class EventListenerWrapper<Sender, Args> {
         this.eventListener = listener;
     }
 
+    /**
+     * Returns the wrapped event listener.
+     * @returns {EventListener<Sender, Args>}
+     */
     get listener(): EventListener<Sender, Args> {
         return this.eventListener;
     }
 
+    /**
+     * Fires the wrapped event listener with the given arguments.
+     * @param sender
+     * @param args
+     */
     fire(sender: Sender, args: Args) {
         this.eventListener(sender, args);
     }
 }
 
+/**
+ * Extends the basic {@link EventListenerWrapper} with rate-limiting functionality.
+ */
 class RateLimitedEventListenerWrapper<Sender, Args> extends EventListenerWrapper<Sender, Args> {
 
     private rateMs: number;
@@ -109,9 +145,11 @@ class RateLimitedEventListenerWrapper<Sender, Args> extends EventListenerWrapper
         this.rateMs = rateMs;
         this.lastFireTime = 0;
 
-        let self = this;
+        // Wrap the event listener with an event listener that does the rate-limiting
         this.rateLimitingEventListener = function (sender: Sender, args: Args) {
             if (Date.now() - this.lastFireTime > this.rateMs) {
+                // Only if enough time since the previous call has passed, call the
+                // actual event listener and record the current time
                 this.fireSuper(sender, args);
                 this.lastFireTime = Date.now();
             }
@@ -119,10 +157,12 @@ class RateLimitedEventListenerWrapper<Sender, Args> extends EventListenerWrapper
     }
 
     private fireSuper(sender: Sender, args: Args) {
+        // Fire the actual external event listener
         super.fire(sender, args);
     }
 
     fire(sender: Sender, args: Args) {
+        // Fire the internal rate-limiting listener instead of the external event listener
         this.rateLimitingEventListener(sender, args);
     }
 }
