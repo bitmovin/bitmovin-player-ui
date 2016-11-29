fefca0152e217373928b991f621688df309df6c9
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2016-11-29 11:06:41 +0100

    Add Object.assign ES5/IE9 polyfill

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -41,6 +41,30 @@ import {SubtitleSelectBox} from "./components/subtitleselectbox";
 import {TitleBar} from "./components/titlebar";
 import {VolumeControlButton} from "./components/volumecontrolbutton";
 
+// Object.assign polyfill for ES5/IE9
+// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+if (typeof Object.assign != 'function') {
+    Object.assign = function(target: any) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
+    };
+}
+
 // Expose classes to window
 // Inspired by https://keestalkstech.com/2016/08/support-both-node-js-and-browser-js-in-one-typescript-file/
 // TODO find out how TS/Browserify can compile the classes to plain JS without the module wrapper we don't need to expose classes to the window scope manually here
