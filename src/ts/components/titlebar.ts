c0f945fc12870d84f31dd0f9bc698bc6822245fd
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-01-09 12:09:08 +0100

    Use metadata label in title bar

--- src/ts/components/titlebar.ts
+++ src/ts/components/titlebar.ts
@@ -1,6 +1,6 @@
 import {Container, ContainerConfig} from './container';
 import {UIManager} from '../uimanager';
-import {LabelConfig, Label} from './label';
+import {MetadataLabel, MetadataLabelContent} from './metadatalabel';
 
 /**
  * Configuration interface for a {@link TitleBar}.
@@ -14,19 +14,16 @@ export interface TitleBarConfig extends ContainerConfig {
  */
 export class TitleBar extends Container<TitleBarConfig> {
 
-  private titleLabel: Label<LabelConfig>;
-  private descriptionLabel: Label<LabelConfig>;
-
   constructor(config: TitleBarConfig = {}) {
     super(config);
-
-    this.titleLabel = new Label({ cssClass: 'ui-titlebar-title' });
-    this.descriptionLabel = new Label({ cssClass: 'ui-titlebar-description' });
 
     this.config = this.mergeConfig(config, {
       cssClass: 'ui-titlebar',
       hidden: true,
-      components: [this.titleLabel, this.descriptionLabel]
+      components: [
+        new MetadataLabel({ content: MetadataLabelContent.Title }),
+        new MetadataLabel({ content: MetadataLabelContent.Description })
+      ]
     }, <TitleBarConfig>this.config);
   }
 
@@ -34,19 +31,6 @@ export class TitleBar extends Container<TitleBarConfig> {
     super.configure(player, uimanager);
 
     let self = this;
-    let uiconfig = uimanager.getConfig();
-
-    if (uiconfig && uiconfig.metadata) {
-      self.titleLabel.setText(uiconfig.metadata.title);
-
-      if (uiconfig.metadata.description) {
-        self.descriptionLabel.setText(uiconfig.metadata.description);
-      }
-    } else {
-      // Cancel configuration if there is no metadata to display
-      // TODO this probably won't work if we put the share buttons into the title bar
-      return;
-    }
 
     uimanager.onControlsShow.subscribe(function () {
       self.show();
