7b4892cf26a7a53f3eee664856e6fa7216e375e1
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-03-16 13:16:34 +0100

    Remove core-js types
    The recently published @types/core-js version 0.9.37 broke the build due to removed definitions, so we replace it with a tsconfig lib update from es5 to es6.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,4 @@
 /// <reference path='player.d.ts' />
-/// <reference path='../../node_modules/@types/core-js/index.d.ts' />
 import {UIManager, UIInstanceManager} from './uimanager';
 import {Button} from './components/button';
 import {ControlBar} from './components/controlbar';
