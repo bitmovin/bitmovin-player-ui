ee6aeecc874fa91fb5053d86428451c9d050905d
Fabrice <fabrice.vaillant@centraliens.net>
2017-06-27 14:02:19 +0200

    Breaks a few line too long

--- src/ts/components/subtitlesettings/characteredgeselectbox.ts
+++ src/ts/components/subtitlesettings/characteredgeselectbox.ts
@@ -15,9 +15,12 @@ export class CharacterEdgeSelectBox extends SubtitleSettingSelectBox {
     super.configure(player, uimanager);
 
     this.addItem('none', 'none');
-    this.addItem('0px 0px 4px rgba(0, 0, 0, 0.9), 0px 1px 4px rgba(0, 0, 0, 0.9), 0px 2px 4px rgba(0, 0, 0, 0.9)', 'raised');
+    this.addItem('0px 0px 4px rgba(0, 0, 0, 0.9), 0px 1px 4px rgba(0, 0, 0, 0.9), 0px 2px 4px rgba(0, 0, 0, 0.9)',
+      'raised');
     this.addItem('rgba(0, 0, 0, 0.8) 0px -2px 1px', 'depressed');
-    this.addItem('-2px 0px 1px rgba(0, 0, 0, 0.8), 2px 0px 1px rgba(0, 0, 0, 0.8), 0px -2px 1px rgba(0, 0, 0, 0.8), 0px 2px 1px rgba(0, 0, 0, 0.8), -1px 1px 1px rgba(0, 0, 0, 0.8), 1px 1px 1px rgba(0, 0, 0, 0.8), 1px -1px 1px rgba(0, 0, 0, 0.8), 1px 1px 1px rgba(0, 0, 0, 0.8)', 'uniform');
+    this.addItem(`-2px 0px 1px rgba(0, 0, 0, 0.8), 2px 0px 1px rgba(0, 0, 0, 0.8), 0px -2px 1px rgba(0, 0, 0, 0.8),
+      0px 2px 1px rgba(0, 0, 0, 0.8), -1px 1px 1px rgba(0, 0, 0, 0.8), 1px 1px 1px rgba(0, 0, 0, 0.8), 
+      1px -1px 1px rgba(0, 0, 0, 0.8), 1px 1px 1px rgba(0, 0, 0, 0.8)`, 'uniform');
     this.addItem('0px 2px 1px rgba(0, 0, 0, 0.8)', 'drop shadowed');
 
     this.selectItem('none');
