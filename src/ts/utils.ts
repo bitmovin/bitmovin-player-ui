f15b28166b5fae6c2f2e384a77b058344be39f68
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-07-12 09:18:57 +0200

    Add reexports for all utils to avoid import errors

---
+++ src/ts/utils.ts
@@ -0,0 +1,12 @@
+/**
+ * All utils were aggregated in this file before being split into separate files.
+ * These reexports serve as backwards compatibility to avoid import errors for users that import utils from here.
+ * TODO remove this file in next major release
+ */
+export {ArrayUtils} from './arrayutils';
+export {BrowserUtils} from './browserutils';
+export {ColorUtils} from './colorutils';
+export {PlayerUtils} from './playerutils';
+export {StorageUtils} from './storageutils';
+export {StringUtils} from './stringutils';
+export {UIUtils} from './uiutils';
