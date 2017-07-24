0f8a4228a5cf1142d61982cb3e5d170ee4639fed
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-07-24 19:18:14 +0200

    Simplify close button

--- src/ts/components/subtitlesettings/subtitlesettingsclosebutton.ts
+++ src/ts/components/subtitlesettings/subtitlesettingsclosebutton.ts
@@ -25,20 +25,4 @@ export class SubtitleSettingsCloseButton extends SubtitleSettingsButton {
       this.settingsPanel.show();
     });
   }
-
-  protected toDomElement(): DOM {
-    // Create the button element with the text label
-    let buttonElement = new DOM('button', {
-      'type': 'button',
-      'id': this.config.id,
-      'class': this.getCssClasses(),
-    }).append(new DOM('span', {}).html(this.config.text));
-
-    // Listen for the click event on the button element and trigger the corresponding event on the button component
-    buttonElement.on('click', () => {
-      this.onClickEvent();
-    });
-
-    return buttonElement;
-  }
 }
