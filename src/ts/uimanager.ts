870eab70b8d352d66cac15a72c96a5e3f12975e4
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-07-31 13:39:17 +0200

    Fix ready check

--- src/ts/uimanager.ts
+++ src/ts/uimanager.ts
@@ -368,7 +368,7 @@ export class UIManager {
 
     // Some components initialize their state on ON_READY. When the UI is loaded after the player is already ready,
     // they will never receive the event so we fire it from here in such cases.
-    if (!player.isReady()) {
+    if (player.isReady()) {
       player.fireEventInUI(player.EVENT.ON_READY, {});
     }
 
