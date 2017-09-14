803068c0b71201d1e036cd8d510a912e6d40c8de
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-09-14 19:53:19 +0200

    Improve subtitle event handling
    Select the correct subtitle when the subtitles list is updated and simplifies the event handlers. Implements GitHub issue #32.

--- src/ts/components/subtitleselectbox.ts
+++ src/ts/components/subtitleselectbox.ts
@@ -17,12 +17,23 @@ export class SubtitleSelectBox extends SelectBox {
   configure(player: bitmovin.PlayerAPI, uimanager: UIInstanceManager): void {
     super.configure(player, uimanager);
 
+    let selectCurrentSubtitle = () => {
+      let currentSubtitle = player.getSubtitle();
+
+      if (currentSubtitle) {
+        this.selectItem(currentSubtitle.id);
+      }
+    };
+
     let updateSubtitles = () => {
       this.clearItems();
 
       for (let subtitle of player.getAvailableSubtitles()) {
         this.addItem(subtitle.id, subtitle.label);
       }
+
+      // Select the correct subtitle after the subtitles have been added
+      selectCurrentSubtitle();
     };
 
     this.onItemSelected.subscribe((sender: SubtitleSelectBox, value: string) => {
@@ -30,23 +41,9 @@ export class SubtitleSelectBox extends SelectBox {
     });
 
     // React to API events
-    player.addEventHandler(player.EVENT.ON_SUBTITLE_ADDED, (event: SubtitleAddedEvent) => {
-      if (this.itemCount() === 0) {
-        // When a subtitle is dynamically added and it is the first one, we add all available subtitles which
-        // includes the "off"-item and makes the subtitle select box visible.
-        updateSubtitles();
-      } else {
-        // If the subtitle select box is already initialized, we just add the new one to the existing ones.
-        this.addItem(event.subtitle.id, event.subtitle.label);
-      }
-    });
-    player.addEventHandler(player.EVENT.ON_SUBTITLE_CHANGED, (event: SubtitleChangedEvent) => {
-      this.selectItem(event.targetSubtitle.id);
-    });
-    player.addEventHandler(player.EVENT.ON_SUBTITLE_REMOVED, (event: SubtitleRemovedEvent) => {
-      this.removeItem(event.subtitleId);
-    });
-
+    player.addEventHandler(player.EVENT.ON_SUBTITLE_ADDED, updateSubtitles);
+    player.addEventHandler(player.EVENT.ON_SUBTITLE_CHANGED, selectCurrentSubtitle);
+    player.addEventHandler(player.EVENT.ON_SUBTITLE_REMOVED, updateSubtitles);
     // Update subtitles when source goes away
     player.addEventHandler(player.EVENT.ON_SOURCE_UNLOADED, updateSubtitles);
     // Update subtitles when a new source is loaded
