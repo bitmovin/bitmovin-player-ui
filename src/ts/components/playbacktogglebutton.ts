cb4704bfa658e6d423dbee42222334b4dc5fc7f2
Kenny <stefan.krainer@bitmovin.com>
2017-10-23 17:21:13 +0200

    listen to the onPlaying event too

--- src/ts/components/playbacktogglebutton.ts
+++ src/ts/components/playbacktogglebutton.ts
@@ -43,6 +43,7 @@ export class PlaybackToggleButton extends ToggleButton<ToggleButtonConfig> {
     // Call handler upon these events
     player.addEventHandler(player.EVENT.ON_PLAY, playbackStateHandler);
     player.addEventHandler(player.EVENT.ON_PAUSED, playbackStateHandler);
+    player.addEventHandler(player.EVENT.ON_PLAYING, playbackStateHandler);
     // when playback finishes, player turns to paused mode
     player.addEventHandler(player.EVENT.ON_PLAYBACK_FINISHED, playbackStateHandler);
     player.addEventHandler(player.EVENT.ON_CAST_STARTED, playbackStateHandler);
