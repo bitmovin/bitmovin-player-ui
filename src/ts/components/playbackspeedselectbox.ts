49a6b285b2e6ab718361d432bd6a30a8c19a184e
Daniel Weinberger <daniel.weinberger@bitmovin.com>
2018-04-06 09:43:10 +0200

    Listen and react to the ON_PLAYBACK_SPEED_CHANGED event to select the correct playback speed.

--- src/ts/components/playbackspeedselectbox.ts
+++ src/ts/components/playbackspeedselectbox.ts
@@ -14,12 +14,7 @@ export class PlaybackSpeedSelectBox extends SelectBox {
   configure(player: bitmovin.PlayerAPI, uimanager: UIInstanceManager): void {
     super.configure(player, uimanager);
 
-    this.addItem('0.25', '0.25x');
-    this.addItem('0.5', '0.5x');
-    this.addItem('1', 'Normal');
-    this.addItem('1.5', '1.5x');
-    this.addItem('2', '2x');
-
+    this.addDefaultItems();
 
     this.onItemSelected.subscribe((sender: PlaybackSpeedSelectBox, value: string) => {
       player.setPlaybackSpeed(parseFloat(value));
@@ -38,5 +33,33 @@ export class PlaybackSpeedSelectBox extends SelectBox {
 
     // when the player hits onReady again, adjust the playback speed selection with fallback to default 1
     player.addEventHandler(player.EVENT.ON_READY, setDefaultValue);
+
+    if (player.EVENT.ON_PLAYBACK_SPEED_CHANGED) {
+      // Since player 7.7.0
+      player.addEventHandler(player.EVENT.ON_PLAYBACK_SPEED_CHANGED, () => {
+        const speed = String(player.getPlaybackSpeed());
+
+        if (!this.selectItem(speed)) {
+          // a playback speed was set which is not in the list, add it to the list to show it to the user
+          this.clearItems();
+          this.addItem(speed, `${speed}x`);
+          this.addDefaultItems();
+          this.selectItem(speed);
+        }
+      });
+    }
+  }
+
+  addDefaultItems() {
+    this.addItem('0.25', '0.25x');
+    this.addItem('0.5', '0.5x');
+    this.addItem('1', 'Normal');
+    this.addItem('1.5', '1.5x');
+    this.addItem('2', '2x');
+  }
+
+  clearItems() {
+    this.items = [];
+    this.selectedItem = null;
   }
 }
