8e53a57b3ba1495662eec6990b292288bb4d8477
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2018-10-25 15:56:14 +0200

    Remove volume/mute state unification code

--- src/ts/components/volumetogglebutton.ts
+++ src/ts/components/volumetogglebutton.ts
@@ -25,14 +25,6 @@ export class VolumeToggleButton extends ToggleButton<ToggleButtonConfig> {
     let muteStateHandler = () => {
       if (player.isMuted()) {
         this.on();
-
-        // When the volume is unmuted and the volume level is veeeery low, we increase it to 10%. This especially helps
-        // in the case when the volume is first turned down to 0 and then the player is muted; when the player gets
-        // unmuted it would switch to volume level 0 which would seem like unmuting did not work, and increasing the
-        // level a bit helps to overcome this issue.
-        if (player.getVolume() < 10) {
-          player.setVolume(10);
-        }
       } else {
         this.off();
       }
@@ -41,14 +33,6 @@ export class VolumeToggleButton extends ToggleButton<ToggleButtonConfig> {
     let volumeLevelHandler = () => {
       const volumeLevelTens = Math.ceil(player.getVolume() / 10);
       this.getDomElement().data(this.prefixCss('volume-level-tens'), String(volumeLevelTens));
-
-      // When the volume is turned down to zero, switch into the mute state of the button. This avoids the usability
-      // issue where the volume is turned down to zero, the button shows the muted icon but is not really unmuted, and
-      // the next button press would switch it into the mute state, visually staying the same which would seem like
-      // an expected unmute did not work.
-      if (volumeLevelTens === 0) {
-        this.off();
-      }
     };
 
     player.addEventHandler(player.EVENT.ON_MUTED, muteStateHandler);
