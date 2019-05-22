b66a129c49f475686f67b0a2329c905905f11922
David Steinacher <david.steinacher@bitmovin.com>
2019-05-22 10:21:07 +0200

    change isMobile constants to getters to fix issues in a non browser env such as server side rendering

--- src/ts/browserutils.ts
+++ src/ts/browserutils.ts
@@ -1,12 +1,18 @@
-export namespace BrowserUtils {
+export class BrowserUtils {
 
   // isMobile only needs to be evaluated once (it cannot change during a browser session)
   // Mobile detection according to Mozilla recommendation: "In summary, we recommend looking for the string “Mobi”
   // anywhere in the User Agent to detect a mobile device."
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
-  export const isMobile = navigator && navigator.userAgent && /Mobi/.test(navigator.userAgent);
+  static get isMobile(): boolean {
+    return navigator && navigator.userAgent && /Mobi/.test(navigator.userAgent);
+  }
 
-  export const isChrome = navigator && navigator.userAgent && /Chrome/.test(navigator.userAgent);
+  static get isChrome(): boolean {
+    return navigator && navigator.userAgent && /Chrome/.test(navigator.userAgent);
+  }
 
-  export const isAndroid = navigator && navigator.userAgent && /Android/.test(navigator.userAgent);
+  static get isAndroid(): boolean {
+    return navigator && navigator.userAgent && /Android/.test(navigator.userAgent);
+  }
 }
