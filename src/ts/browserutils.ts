b1c5bd00f7e336a0e72b2ffd8a61b715baaddb2c
David Steinacher <david.steinacher@bitmovin.com>
2019-05-22 10:27:59 +0200

    add check if window object exists

--- src/ts/browserutils.ts
+++ src/ts/browserutils.ts
@@ -5,14 +5,27 @@ export class BrowserUtils {
   // anywhere in the User Agent to detect a mobile device."
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
   static get isMobile(): boolean {
+    if (!this.windowExists()) {
+      return false;
+    }
     return navigator && navigator.userAgent && /Mobi/.test(navigator.userAgent);
   }
 
   static get isChrome(): boolean {
+    if (!this.windowExists()) {
+      return false;
+    }
     return navigator && navigator.userAgent && /Chrome/.test(navigator.userAgent);
   }
 
   static get isAndroid(): boolean {
+    if (!this.windowExists()) {
+      return false;
+    }
     return navigator && navigator.userAgent && /Android/.test(navigator.userAgent);
+  }
+
+  private static windowExists(): boolean {
+    return typeof window !== 'undefined';
   }
 }
