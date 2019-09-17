a3087c6749028f88ec44170f50dbcc6e5bf56b99
unki2aut <martin.fillafer@bitmovin.com>
2019-09-17 16:38:43 +0200

    Remove subtitleClearHandlers on actions that are handled by the player

--- src/ts/components/subtitleoverlay.ts
+++ src/ts/components/subtitleoverlay.ts
@@ -92,11 +92,8 @@ export class SubtitleOverlay extends Container<ContainerConfig> {
       this.updateComponents();
     };
 
-    player.on(player.exports.PlayerEvent.AudioChanged, subtitleClearHandler);
     player.on(player.exports.PlayerEvent.SubtitleEnabled, subtitleClearHandler);
     player.on(player.exports.PlayerEvent.SubtitleDisabled, subtitleClearHandler);
-    player.on(player.exports.PlayerEvent.Seek, subtitleClearHandler);
-    player.on(player.exports.PlayerEvent.TimeShift, subtitleClearHandler);
     player.on(player.exports.PlayerEvent.PlaybackFinished, subtitleClearHandler);
     player.on(player.exports.PlayerEvent.SourceUnloaded, subtitleClearHandler);
 
