9e3d2d3cbd10f49e5016e27ae8903586afcd6b69
Hannes Linder <hannes.linder@bitmovin.com>
2019-09-19 17:14:02 +0200

    Rename default subtitle class

--- src/ts/components/subtitleoverlay.ts
+++ src/ts/components/subtitleoverlay.ts
@@ -20,7 +20,7 @@ export class SubtitleOverlay extends Container<ContainerConfig> {
 
   private static readonly CLASS_CONTROLBAR_VISIBLE = 'controlbar-visible';
   private static readonly CLASS_CEA_608 = 'cea608';
-  private static readonly CLASS_DEFAULT_SUBTITILE_POSITION = 'default-subtitle-position';
+  private static readonly CLASS_DEFAULT_SUBTITILE_POSITION = 'subtitle-position-default';
 
   // The number of rows in a cea608 grid
   private static readonly CEA608_NUM_ROWS = 15;
