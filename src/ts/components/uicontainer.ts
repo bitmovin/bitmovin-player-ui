e65dcfe6382f39b841ce9fd5cc5b376f5cb6af84
David Steinacher <david.steinacher@bitmovin.com>
2019-09-23 15:28:19 +0200

    lower hideDelay to 2 seconds

--- src/ts/components/uicontainer.ts
+++ src/ts/components/uicontainer.ts
@@ -41,7 +41,7 @@ export class UIContainer extends Container<UIContainerConfig> {
 
     this.config = this.mergeConfig(config, <UIContainerConfig>{
       cssClass: 'ui-uicontainer',
-      hideDelay: 5000,
+      hideDelay: 2000,
       hidePlayerStateExceptions: [
         PlayerUtils.PlayerState.PREPARED,
         PlayerUtils.PlayerState.PAUSED,
