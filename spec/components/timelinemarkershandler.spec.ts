0f6c2181017448e432f4124f20de6e16a299d461
Adis Talic <adis.talic@bitmovin.com>
2020-07-15 16:21:19 +0200

    Group tests in foreach

--- spec/components/timelinemarkershandler.spec.ts
+++ spec/components/timelinemarkershandler.spec.ts
@@ -27,65 +27,62 @@ describe('TimelineMarkersHandler', () => {
     uimanagerMock = MockHelper.getUiInstanceManagerMock();
   });
 
-  it('should return TimelineMarker for VOD', () => {
-    const expectedMarker = getTimelineMarker(4);
-    uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
-    jest.spyOn(playerMock, 'getSeekableRange').mockReturnValue({start: 0, end: 10});
-    jest.spyOn(playerMock, 'isLive').mockReturnValue(false);
-    jest.spyOn(playerMock, 'getDuration').mockReturnValue(10);
+  describe('TimelineMarkers VOD', () => {
+    it('should return TimelineMarker', () => {
+      const expectedMarker = getTimelineMarker(4);
+      uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
+      jest.spyOn(playerMock, 'getSeekableRange').mockReturnValue({start: 0, end: 10});
+      jest.spyOn(playerMock, 'isLive').mockReturnValue(false);
+      jest.spyOn(playerMock, 'getDuration').mockReturnValue(10);
 
-    timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
-    const marker = timelineMarkersHandlerMock.getMarkerAtPosition(41);
+      timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
+      const marker = timelineMarkersHandlerMock.getMarkerAtPosition(41);
 
-    expect(marker).not.toBeUndefined();
-    expect(marker.marker).toEqual(expectedMarker);
+      expect(marker).not.toBeUndefined();
+      expect(marker.marker).toEqual(expectedMarker);
+    });
   });
 
-  it('should return TimelineMarker for LIVE', () => {
-    const expectedMarker = getTimelineMarker(180);
-    uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
-    jest.spyOn(playerMock, 'getCurrentTime').mockReturnValue(200);
-    jest.spyOn(playerMock, 'getTimeShift').mockReturnValue(0);
-    jest.spyOn(playerMock, 'getMaxTimeShift').mockReturnValue(-100);
-    jest.spyOn(playerMock, 'isLive').mockReturnValue(true);
-    jest.spyOn(playerMock, 'getDuration').mockReturnValue(Infinity);
+  describe('TimelineMarkers LIVE', () => {
+    beforeEach(() => {
+      jest.spyOn(playerMock, 'getCurrentTime').mockReturnValue(200);
+      jest.spyOn(playerMock, 'getTimeShift').mockReturnValue(0);
+      jest.spyOn(playerMock, 'getMaxTimeShift').mockReturnValue(-100);
+      jest.spyOn(playerMock, 'isLive').mockReturnValue(true);
+      jest.spyOn(playerMock, 'getDuration').mockReturnValue(Infinity);
+    });
 
-    timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
-    const marker = timelineMarkersHandlerMock.getMarkerAtPosition(81);
+    it('should return TimelineMarker', () => {
+      const expectedMarker = getTimelineMarker(180);
+      uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
 
-    expect(marker).not.toBeUndefined();
-    expect(marker.marker).toEqual(expectedMarker);
+
+      timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
+      const marker = timelineMarkersHandlerMock.getMarkerAtPosition(81);
+
+      expect(marker).not.toBeUndefined();
+      expect(marker.marker).toEqual(expectedMarker);
+    });
+
+    it('should remove TimelineMarker that are outside timeshift range (Passed)', () => {
+      const expectedMarker = getTimelineMarker(90);
+      uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
+
+      timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
+      const markers = uimanagerMock.getConfig().metadata.markers;
+
+      expect(markers).toHaveLength(0);
+    });
+
+    it('should not display TimelineMarkers that are outside timeshift range (Upcoming)', () => {
+      const expectedMarker = getTimelineMarker(220);
+      uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
+
+      timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
+      const markers = uimanagerMock.getConfig().metadata.markers;
+
+      expect(DOMMock.append).not.toHaveBeenCalled();
+      expect(markers).toHaveLength(1);
+    });
   });
-
-  it('should remove TimelineMarker that are outside timeshift range (Passed)', () => {
-    const expectedMarker = getTimelineMarker(90);
-    uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
-    jest.spyOn(playerMock, 'getCurrentTime').mockReturnValue(200);
-    jest.spyOn(playerMock, 'getTimeShift').mockReturnValue(0);
-    jest.spyOn(playerMock, 'getMaxTimeShift').mockReturnValue(-100);
-    jest.spyOn(playerMock, 'isLive').mockReturnValue(true);
-    jest.spyOn(playerMock, 'getDuration').mockReturnValue(Infinity);
-
-    timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
-    const markers = uimanagerMock.getConfig().metadata.markers;
-
-    expect(markers).toHaveLength(0);
-  });
-
-  it('should not display TimelineMarkers that are outside timeshift range (Upcoming)', () => {
-    const expectedMarker = getTimelineMarker(220);
-    uimanagerMock.getConfig().metadata.markers.push(expectedMarker);
-    jest.spyOn(playerMock, 'getCurrentTime').mockReturnValue(200);
-    jest.spyOn(playerMock, 'getTimeShift').mockReturnValue(0);
-    jest.spyOn(playerMock, 'getMaxTimeShift').mockReturnValue(-100);
-    jest.spyOn(playerMock, 'isLive').mockReturnValue(true);
-    jest.spyOn(playerMock, 'getDuration').mockReturnValue(Infinity);
-
-    timelineMarkersHandlerMock.initialize(playerMock, uimanagerMock);
-    const markers = uimanagerMock.getConfig().metadata.markers;
-
-    expect(DOMMock.append).not.toHaveBeenCalled();
-    expect(markers).toHaveLength(1);
-  });
-
 });
