a287c12ee831da6fb7583d6141e04fab190f70d7
Adis Talic <adis.talic@bitmovin.com>
2020-07-29 15:14:28 +0200

    Remove prefixing

--- src/ts/components/timelinemarkershandler.ts
+++ src/ts/components/timelinemarkershandler.ts
@@ -139,10 +139,6 @@ export class TimelineMarkersHandler {
 
     const positionInPx = (seekBarWidthPx / 100) * (marker.position < 0 ? 0 : marker.position);
     const cssProperties: { [propertyName: string]: string } = {
-      '-webkit-transform': `translateX(${positionInPx}px)`,
-      '-moz-transform': `translateX(${positionInPx}px)`,
-      '-ms-transform': `translateX(${positionInPx}px)`,
-      '-o-transform': `translateX(${positionInPx}px)`,
       'transform': `translateX(${positionInPx}px)`,
     };
 
