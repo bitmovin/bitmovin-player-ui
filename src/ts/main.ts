f36c89c2730d64ea1606d4043d3d305c7467e490
cagin <nebu.gomm@gmail.com>
2020-11-11 12:15:27 +0100

    moved polyfills into a separate file

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -79,55 +79,6 @@ export { SettingsPanelPageBackButton } from './components/settingspanelpagebackb
 export { SettingsPanelPageOpenButton } from './components/settingspanelpageopenbutton';
 export { SubtitleSettingsPanelPage } from './components/subtitlesettings/subtitlesettingspanelpage';
 export { SettingsPanelItem } from './components/settingspanelitem';
+import { installPolyfills } from './polyfills';
 
-// Object.assign polyfill for ES5/IE9
-// https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-if (typeof Object.assign !== 'function') {
-  Object.assign = function(target: any) {
-    'use strict';
-    if (target == null) {
-      throw new TypeError('Cannot convert undefined or null to object');
-    }
-
-    target = Object(target);
-    for (let index = 1; index < arguments.length; index++) {
-      let source = arguments[index];
-      if (source != null) {
-        for (let key in source) {
-          if (Object.prototype.hasOwnProperty.call(source, key)) {
-            target[key] = source[key];
-          }
-        }
-      }
-    }
-    return target;
-  };
-}
-
-/*
-* Polyfill for Array Array.prototype.find
-* source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-*/
-if (!Array.prototype.find) {
-    Array.prototype.find = function(predicate: any) {
-      'use strict';
-      if (this == null) {
-        throw new TypeError('Array.prototype.find called on null or undefined');
-      }
-      if (typeof predicate !== 'function') {
-        throw new TypeError('predicate must be a function');
-      }
-      var list = Object(this);
-      var length = list.length >>> 0;
-      var thisArg = arguments[1];
-      var value;
-
-      for (var i = 0; i < length; i++) {
-        value = list[i];
-        if (predicate.call(thisArg, value, i, list)) {
-          return value;
-        }
-      }
-      return undefined;
-    };
-}
+installPolyfills();
---
+++ src/ts/polyfills.ts
@@ -0,0 +1,55 @@
+export function installPolyfills() {
+
+  // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+  if (typeof Object.assign !== 'function') {
+    Object.assign = function(target: any) {
+      'use strict';
+      if (target == null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      target = Object(target);
+      for (let index = 1; index < arguments.length; index++) {
+        let source = arguments[index];
+        if (source != null) {
+          for (let key in source) {
+            if (Object.prototype.hasOwnProperty.call(source, key)) {
+              target[key] = source[key];
+            }
+          }
+        }
+      }
+      return target;
+    };
+  }
+
+  /*
+  * Polyfill for Array Array.prototype.find
+  * source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+  */
+  if (!Array.prototype.find) {
+      Array.prototype.find = function(predicate: any) {
+        'use strict';
+        if (this == null) {
+          throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+          value = list[i];
+          if (predicate.call(thisArg, value, i, list)) {
+            return value;
+          }
+        }
+        return undefined;
+      };
+  }
+}
