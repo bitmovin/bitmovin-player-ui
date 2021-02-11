4891b5975a8e36831229f69d7202b88e913a355b
Jameson Steiner <jameson.steiner@gmail.com>
2021-02-12 00:01:58 +0100

    Introduce FocusVisibilityTracker to manage visible focus for UI elements
    Visibility of UI elements' focus is now tied to a 'bpmui-focus-visible' CSS class that gets added to any UI element receiving focus via a keyboard interaction. Elements whose focus resulted from a mouse/pointer/touch interaction won't get this class causing their focus to not be indicated to the user.

---
+++ src/ts/focusvisibilitytracker.ts
@@ -0,0 +1,71 @@
+const FocusVisibleClassName = '{{PREFIX}}-focus-visible';
+
+export class FocusVisibilityTracker {
+  private readonly eventHandlerMap: { [eventName: string]: EventListenerOrEventListenerObject };
+  private lastInteractionWasKeyboard: boolean = true;
+
+  constructor(document: Document, private bitmovinUiPrefix: string) {
+    this.eventHandlerMap = {
+      mousedown: this.onMouseOrPointerOrTouch,
+      pointerdown: this.onMouseOrPointerOrTouch,
+      touchstart: this.onMouseOrPointerOrTouch,
+      keydown: this.onKeyDown,
+      focus: this.onFocus,
+      blur: this.onBlur,
+    };
+    this.registerEventListeners(document);
+  }
+
+  private onKeyDown = (e: KeyboardEvent) => {
+    if (e.metaKey || e.altKey || e.ctrlKey) {
+      return;
+    }
+
+    this.lastInteractionWasKeyboard = true;
+  };
+
+  private onMouseOrPointerOrTouch = () => (this.lastInteractionWasKeyboard = false);
+
+  private onFocus = ({ target: element }: FocusEvent) => {
+    if (
+      this.lastInteractionWasKeyboard &&
+      isHtmlElement(element) &&
+      isBitmovinUi(element, this.bitmovinUiPrefix) &&
+      !element.classList.contains(FocusVisibleClassName)
+    ) {
+      element.classList.add(FocusVisibleClassName);
+    }
+  };
+
+  private onBlur = ({ target: element }: FocusEvent) => {
+    if (isHtmlElement(element)) {
+      element.classList.remove(FocusVisibleClassName);
+    }
+  };
+
+  private registerEventListeners(document: Document): void {
+    for (const event in this.eventHandlerMap) {
+      document.addEventListener(event, this.eventHandlerMap[event], true);
+    }
+  }
+
+  private unregisterEventListeners(): void {
+    for (const event in this.eventHandlerMap) {
+      document.removeEventListener(event, this.eventHandlerMap[event], true);
+    }
+  }
+
+  public release(): void {
+    this.unregisterEventListeners();
+  }
+}
+
+function isBitmovinUi(element: Element, bitmovinUiPrefix: string): boolean {
+  return element.id.indexOf(bitmovinUiPrefix) === 0;
+}
+
+function isHtmlElement(element: unknown): element is Element & { classList: DOMTokenList } {
+  return (
+    element instanceof HTMLElement && element.classList instanceof DOMTokenList
+  );
+}
