7c03a9ca09143933d26e0b74d2bf507ef55aa249
dweinber <daniel.weinberger@bitmovin.com>
2023-01-02 13:07:30 +0100

    Export Spatial Navigation objects and types so they can be used in externally configured UIs

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -14,6 +14,11 @@ export { StorageUtils } from './storageutils';
 export { ErrorUtils } from './errorutils';
 // Localization
 export { i18n } from './localization/i18n';
+// Spatial Navigation
+export { SpatialNavigation } from './spatialnavigation/spatialnavigation';
+export { NavigationGroup } from './spatialnavigation/navigationgroup';
+export { RootNavigationGroup } from './spatialnavigation/rootnavigationgroup';
+export { ListNavigationGroup, ListOrientation } from './spatialnavigation/ListNavigationGroup';
 // Components
 export { Button } from './components/button';
 export { ControlBar } from './components/controlbar';
