2ef844b4c26257236d0ce5584e17a90d3f3f2dc8
Andreas Kogler <andy.kogler.junior@gmail.com>
2023-03-13 09:25:13 +0100

    Introduce methods for subtitle overflow detection & handling

--- src/ts/components/subtitleoverlay.ts
+++ src/ts/components/subtitleoverlay.ts
@@ -10,6 +10,13 @@ import { i18n } from '../localization/i18n';
 import { VttUtils } from '../vttutils';
 import { VTTProperties } from 'bitmovin-player/types/subtitles/vtt/API';
 
+interface SubtitleCropDetectionResult {
+  top: boolean;
+  right: boolean;
+  bottom: boolean;
+  left: boolean;
+}
+
 /**
  * Overlays the player to display subtitles.
  */
@@ -128,6 +135,49 @@ export class SubtitleOverlay extends Container<ContainerConfig> {
     this.configureCea608Captions(player, uimanager);
     // Init
     subtitleClearHandler();
+  }
+
+  detectCroppedSubtitleLabel(
+    labelElement: HTMLElement,
+  ): SubtitleCropDetectionResult {
+    const parent = this.getDomElement().get(0);
+
+    const childRect = labelElement.getBoundingClientRect();
+    const parentRect = parent.getBoundingClientRect();
+
+    return {
+      top: childRect.top < parentRect.top,
+      right: childRect.right > parentRect.right,
+      bottom: childRect.bottom > parentRect.bottom,
+      left: childRect.left < parentRect.left,
+    };
+  }
+
+  handleSubtitleCropping(label: SubtitleLabel) {
+    const labelDomElement = label.getDomElement();
+    const cropDetection = this.detectCroppedSubtitleLabel(
+      labelDomElement.get(0),
+    );
+
+    if (cropDetection.top) {
+      labelDomElement.css('top', '0');
+      labelDomElement.css('bottom', 'unset');
+    }
+
+    if (cropDetection.right) {
+      labelDomElement.css('right', '0');
+      labelDomElement.css('left', 'unset');
+    }
+
+    if (cropDetection.bottom) {
+      labelDomElement.css('bottom', '0');
+      labelDomElement.css('top', 'unset');
+    }
+
+    if (cropDetection.left) {
+      labelDomElement.css('left', '0');
+      labelDomElement.css('right', 'unset');
+    }
   }
 
   generateLabel(event: SubtitleCueEvent): SubtitleLabel {
