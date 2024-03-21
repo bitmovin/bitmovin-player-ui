e251e5c58498dd60d7a801c5be335c282d0236f9
mukulmishra18 <mkmishra1895@gmail.com>
2024-03-21 16:16:42 +0530

    Add version compare link in the changelog

--- .github/scripts/updateChangelog.js
+++ .github/scripts/updateChangelog.js
@@ -6,12 +6,19 @@
  * @param {string} releaseDate the release date to be written to the changelog
  */
 function updateChangeLog(changelogString, version, releaseDate) {
-    const optionalBetaOrRc = '(-rc.d+)?(-(b|beta).d+)?';
-    const changelogVersionRegExp = new RegExp(
-      `\\[(development|develop|unreleased|${version})${optionalBetaOrRc}.*`,
-      'gi',
-    );
-    return changelogString.replace(changelogVersionRegExp, `[${version}] - ${releaseDate}`);
-  }
-  
-  module.exports.updateChangeLog = updateChangeLog;
+  const optionalBetaOrRc = '(-rc.d+)?(-(b|beta).d+)?';
+  const changelogVersionRegExp = new RegExp(
+    `\\[(development|develop|unreleased|${version})${optionalBetaOrRc}.*`,
+    'gi',
+  );
+
+  const lastReleaseVersion = changelogString.match(/\[(\d+.\d+.\d+)\]/)[1];
+  changelogString = changelogString.replace(changelogVersionRegExp, `[${version}] - ${releaseDate}`);
+
+  return changelogString.replace(
+    '## 1.0.0 (2017-02-03)\n- First release\n\n',
+    `## 1.0.0 (2017-02-03)\n- First release\n\n[${version}]: https://github.com/bitmovin/bitmovin-player-ui/compare/v${version}...v${lastReleaseVersion}\n`
+  );
+}
+
+module.exports.updateChangeLog = updateChangeLog;
