58d5a9aae07ca334eb87abfb395b8ba1fd82e4f8
Andreas Kogler <andy.kogler.junior@gmail.com>
2024-06-05 16:07:36 +0200

    Use ES6 import instead of `player.exports`

--- src/ts/components/ecomodecontainer.ts
+++ src/ts/components/ecomodecontainer.ts
@@ -1,4 +1,4 @@
-import { PlayerAPI, SegmentPlaybackEvent, VideoQuality } from 'bitmovin-player';
+import { PlayerAPI, PlayerEvent, SegmentPlaybackEvent, VideoQuality } from 'bitmovin-player';
 import { i18n } from '../localization/i18n';
 import { Container, ContainerConfig } from './container';
 import { EcoModeToggleButton } from './ecomodetogglebutton';
@@ -52,7 +52,7 @@ export class EcoModeContainer extends Container<ContainerConfig> {
   }
 
   configure(player: PlayerAPI): void {
-    player.on(player.exports.PlayerEvent.SegmentPlayback, (segment: SegmentPlaybackEvent) => {
+    player.on(PlayerEvent.SegmentPlayback, (segment: SegmentPlaybackEvent) => {
       if (!segment.mimeType.includes('video')) {
         return;
       }
