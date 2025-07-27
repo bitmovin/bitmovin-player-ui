be8ff2dba2907c19a138f28be82e19decf843368
Michele Pozzi <123.mpozzi@gmail.com>
2025-07-27 13:03:43 +0200

    Remove extra white line

--- src/ts/components/seekbar/SeekBar.ts
+++ src/ts/components/seekbar/SeekBar.ts
@@ -1118,7 +1118,6 @@ export class SeekBar extends Component<SeekBarConfig> {
   }
 
   protected onSeekedEvent(percentage: number) {
-
     this.clearAllThickenedMarkers();
     this.seekBarEvents.onSeeked.dispatch(this, percentage);
   }
