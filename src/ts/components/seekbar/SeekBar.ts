23f1cd9ff8a324e20cf5247f2cef479e0b7ffa70
Michele Pozzi <123.mpozzi@gmail.com>
2025-07-29 12:02:47 +0200

    Only allow markers to overflow, and prevent it for the rest of the bars

--- src/ts/components/seekbar/SeekBar.ts
+++ src/ts/components/seekbar/SeekBar.ts
@@ -747,9 +747,9 @@ export class SeekBar extends Component<SeekBarConfig> {
     this.seekBarMarkersContainer = seekBarChapterMarkersContainer;
 
     seekBarBarsContainer.append(this.seekBarBackdrop, this.seekBarBufferPosition, this.seekBarSeekPosition,
-      this.seekBarPlaybackPosition, this.seekBarMarkersContainer);
+      this.seekBarPlaybackPosition);
 
-    seekBar.append(seekBarBarsContainer, this.seekBarPlaybackPositionMarker);
+    seekBar.append(seekBarBarsContainer, this.seekBarMarkersContainer, this.seekBarPlaybackPositionMarker);
 
     let seeking = false;
 
