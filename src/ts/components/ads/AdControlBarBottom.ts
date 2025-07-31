0b13ec3b7cb2880f08d4e5860db592ceec516ce0
Michele Pozzi <123.mpozzi@gmail.com>
2025-07-31 16:36:48 +0200

    Extract falling/slide down (non hidden) animation style into mixin

--- src/ts/components/ads/AdControlBarBottom.ts
+++ src/ts/components/ads/AdControlBarBottom.ts
@@ -10,6 +10,9 @@ import { UIInstanceManager } from '../../UIManager';
  * A container for ad-specific bottom control bar components.
  */
 export class AdControlBarBottom extends Container<ContainerConfig> {
+  private static readonly CLASS_TOP_AD_CONTROL_BAR = 'ad-controlbar-top';
+  private static readonly CLASS_BOTTOM_HIDDEN = 'bottom-hidden';
+
   constructor(config: Partial<ContainerConfig> = {}) {
     super({
       ...config,
@@ -26,21 +29,18 @@ export class AdControlBarBottom extends Container<ContainerConfig> {
   hide(): void {
     super.hide();
 
-    const controlBarTop = document.querySelector('.bmpui-ui-controlbar .ad-controlbar-top') as HTMLElement;
-    console.log(controlBarTop);
+    const controlBarTop = document.querySelector(`.${AdControlBarBottom.CLASS_TOP_AD_CONTROL_BAR}`) as HTMLElement;
     if (controlBarTop) {
-      controlBarTop.style.transition = 'transform 0.35s ease';
-      controlBarTop.style.transform = 'translateY(30px)';
+      controlBarTop.classList.add(this.prefixCss(AdControlBarBottom.CLASS_BOTTOM_HIDDEN));
     }
   }
 
   show(): void {
     super.show();
 
-    const controlBarTop = document.querySelector('.bmpui-ui-controlbar .ad-controlbar-top') as HTMLElement;
+    const controlBarTop = document.querySelector(`.${AdControlBarBottom.CLASS_TOP_AD_CONTROL_BAR}`) as HTMLElement;
     if (controlBarTop) {
-      controlBarTop.style.transition = 'transform 0.35s ease';
-      controlBarTop.style.transform = 'translateY(0)';
+      controlBarTop.classList.remove(this.prefixCss(AdControlBarBottom.CLASS_BOTTOM_HIDDEN));
     }
   }
 
