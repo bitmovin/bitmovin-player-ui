cfb570228462a0006ca10ee2f41590f2f4344785
Jameson Steiner <jameson.steiner@gmail.com>
2025-08-12 13:41:12 +0200

    Fix font size not changing for CEA captions

--- src/ts/components/subtitleoverlay.ts
+++ src/ts/components/subtitleoverlay.ts
@@ -320,7 +320,7 @@ export class SubtitleOverlay extends Container<ContainerConfig> {
     // Flag telling if the CEA-608 mode is enabled
     this.cea608Enabled = false;
     // Track last known dimensions to avoid unnecessary recalculations
-    let lastCeaGridRecalculation = { overlayWidth: 0, overlayHeight: 0 };
+    let lastCeaGridRecalculation = { overlayWidth: 0, overlayHeight: 0, fontSizeFactor: 0 };
 
     const settingsManager = uimanager.getSubtitleSettingsManager();
     if (settingsManager.fontSize.value != null) {
@@ -350,13 +350,18 @@ export class SubtitleOverlay extends Container<ContainerConfig> {
       const currentHeight = overlayElement.height();
       const hasOverlaySizeChanged = currentWidth !== lastCeaGridRecalculation.overlayWidth ||
         currentHeight !== lastCeaGridRecalculation.overlayHeight;
-      
-      if (!hasOverlaySizeChanged) {
-        // subtitle overlay dimensions have not changed, no need to recalculate
+      const hasFontSizeFactorChanged = this.FONT_SIZE_FACTOR !== lastCeaGridRecalculation.fontSizeFactor;
+
+      if (!hasOverlaySizeChanged && !hasFontSizeFactorChanged) {
+        // none of the input variables changed, no need to recalculate
         return;
       }
       
-      lastCeaGridRecalculation = { overlayWidth: currentWidth, overlayHeight: currentHeight };
+      lastCeaGridRecalculation = {
+        overlayWidth: currentWidth,
+        overlayHeight: currentHeight,
+        fontSizeFactor: this.FONT_SIZE_FACTOR,
+      };
       
       const dummyLabel = new SubtitleLabel({ text: 'X' });
       dummyLabel.getDomElement().css({
