addffeffe7eb0d93f694ebdbd157797cea9a569d
David Steinacher <david.steinacher@bitmovin.com>
2025-08-20 10:48:19 +0200

    remove ListOrientation

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -18,7 +18,7 @@ export { i18n, I18n, Vocabulary, Vocabularies, CustomVocabulary, LocalizableText
 export { SpatialNavigation } from './spatialnavigation/SpatialNavigation';
 export { NavigationGroup } from './spatialnavigation/NavigationGroup';
 export { RootNavigationGroup } from './spatialnavigation/RootNavigationGroup';
-export { SettingsPanelNavigationGroup, ListOrientation } from './spatialnavigation/SettingsPanelNavigationGroup';
+export { SettingsPanelNavigationGroup } from './spatialnavigation/SettingsPanelNavigationGroup';
 // Components
 export { Button, ButtonConfig } from './components/buttons/Button';
 export { ControlBar, ControlBarConfig } from './components/ControlBar';
