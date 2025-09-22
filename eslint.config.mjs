c785480a4020e1e4897059f019764bdeac56749d
Daniel Weinberger <dweinber@users.noreply.github.com>
2025-09-22 13:35:48 +0200

    Add prettier config to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,25 +4,24 @@ import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default tseslint.config({
-  files: [
-    'src/**/*.ts',
-    'spec/**/*.ts',
-  ],
+  files: ['src/**/*.ts', 'spec/**/*.ts'],
   extends: [
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
     {
       languageOptions: {
         parserOptions: {
-          projectService: true,
+          project: 'eslint-tsconfig.json',
           tsconfigRootDir: __dirname,
         },
       },
     },
+    prettier, // needs to be last in the extends array
   ],
   rules: {
     'no-prototype-builtins': 'off',
@@ -42,8 +41,11 @@ export default tseslint.config({
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-redundant-type-constituents': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
-    '@typescript-eslint/no-unused-expressions': ['error', {
-      allowTernary: true,
-    }]
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowTernary: true,
+      },
+    ],
   },
 });
