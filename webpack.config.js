af7e86cb4a37d28f09cb2c3f5d47dc7dccfd3637
Daniel Weinberger <dweinber@users.noreply.github.com>
2025-09-25 10:48:05 +0200

    Add start script and config to use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   entry: {
     [outputnames.filename]: {
       import: ['./src/scss/bitmovinplayer-ui.scss', './src/ts/main.ts'],
-      filename: '[name].js',
+      filename: './js/[name].js',
       library: {
         type: 'umd',
         name: {
@@ -73,13 +73,13 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/html/index.html',
-      filename: '../index.html',
+      filename: './index.html',
       inject: false,
       minify: false,
     }),
     new HtmlWebpackPlugin({
       template: './src/html/simple.html',
-      filename: '../simple.html',
+      filename: './simple.html',
       inject: false,
       minify: false,
     }),
@@ -88,9 +88,22 @@ module.exports = {
     extensions: ['.ts', '.js', '.scss', '.css'],
   },
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '',
     clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 9000,
+    hot: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
   },
   target: ['web', 'es5'],
 };
