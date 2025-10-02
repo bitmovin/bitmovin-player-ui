152fd9467ff167b7973b6c7e0cef0a5e3f2b2ef8
Daniel Weinberger <dweinber@users.noreply.github.com>
2025-10-02 14:03:14 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -149,7 +149,7 @@ module.exports = (env, { mode }) => {
 };
 
 async function createJavascriptUiFrameworkFilesWithReplacedPrefix() {
-  await exec('npx tsc'); // run the typescript compiler to generate the dist-tsc folder
+  await exec('npx tsc'); // run the typescript compiler to generate the individual UI Framework JS files in `dist/js/framework/`
   const options = {
     templatingOn: false, // disable liquid syntax to avoid issues with {{ and }}
     find: '{{PREFIX}}',
