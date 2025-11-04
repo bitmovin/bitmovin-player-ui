d6f9ea746472eaac9f9888bdfb5bcbb1689fb278
David Steinacher <david.steinacher@bitmovin.com>
2025-11-04 16:44:09 +0100

    add possibilty to configure an AdMessageLabel with a static text

--- src/ts/components/ads/AdMessageLabel.ts
+++ src/ts/components/ads/AdMessageLabel.ts
@@ -1,9 +1,11 @@
-import { UIInstanceManager } from '../../UIManager';
-import { AdEvent, LinearAd, PlayerAPI } from 'bitmovin-player';
 import { Label, LabelConfig } from '../labels/Label';
+import { i18n } from '../../localization/i18n';
+import { AdEvent, LinearAd, PlayerAPI } from 'bitmovin-player';
+import { UIInstanceManager } from '../../UIManager';
+import { StringUtils } from '../../utils/StringUtils';
 
 /**
- * A label that displays a message regarding the ad that's currently being played.
+ * A label that displays a message about a running ad, optionally with a countdown.
  *
  * @category Labels
  */
@@ -23,24 +25,29 @@ export class AdMessageLabel extends Label<LabelConfig> {
   configure(player: PlayerAPI, uimanager: UIInstanceManager): void {
     super.configure(player, uimanager);
 
-    const clearText = () => {
-      this.setText('');
+    let config = this.getConfig();
+    let text = config.text;
+
+    let updateMessageHandler = () => {
+      this.setText(StringUtils.replaceAdMessagePlaceholders(i18n.performLocalization(text), null, player));
     };
 
-    clearText();
+    let adStartHandler = (event: AdEvent) => {
+      let uiConfig = (event.ad as LinearAd).uiConfig;
+      text = (uiConfig && uiConfig.message) || config.text;
 
-    player.on(player.exports.PlayerEvent.SourceUnloaded, clearText);
-    player.on(player.exports.PlayerEvent.AdError, clearText);
-    player.on(player.exports.PlayerEvent.AdSkipped, clearText);
-    player.on(player.exports.PlayerEvent.AdFinished, clearText);
-    player.on(player.exports.PlayerEvent.AdStarted, event => {
-      const ad = (event as AdEvent).ad;
-      if (!ad.isLinear) {
-        return;
-      }
+      updateMessageHandler();
 
-      const linearAd = ad as LinearAd;
-      this.setText(linearAd.uiConfig?.message ?? '');
-    });
+      player.on(player.exports.PlayerEvent.TimeChanged, updateMessageHandler);
+    };
+
+    let adEndHandler = () => {
+      player.off(player.exports.PlayerEvent.TimeChanged, updateMessageHandler);
+    };
+
+    player.on(player.exports.PlayerEvent.AdStarted, adStartHandler);
+    player.on(player.exports.PlayerEvent.AdSkipped, adEndHandler);
+    player.on(player.exports.PlayerEvent.AdError, adEndHandler);
+    player.on(player.exports.PlayerEvent.AdFinished, adEndHandler);
   }
 }
