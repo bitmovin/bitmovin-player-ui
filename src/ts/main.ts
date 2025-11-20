5fe2484d404e475bc6497f8671fa3ed8816081ae
Simon Oberhammer <simon.oberhammer@orf.at>
2025-11-20 13:58:55 +0100

    fix non existent type export

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -135,7 +135,7 @@ export {
   ListItemLabelTranslator,
 } from './components/lists/ListSelector';
 export { AdStatusOverlay } from './components/ads/AdStatusOverlay';
-export { Icon, IconConfig } from './components/Icon';
+export { Icon } from './components/Icon';
 
 // Object.assign polyfill for ES5/IE9
 // https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
