268f24f212b5d6a256eed4b70a3d701b0b212754
Simon Oberhammer <simon.oberhammer@orf.at>
2025-12-05 07:59:36 +0100

    remove stop propagation for focus & click in button

--- src/ts/components/buttons/Button.ts
+++ src/ts/components/buttons/Button.ts
@@ -55,20 +55,6 @@ export interface ButtonConfig extends ComponentConfig {
    * Default: {@link ButtonStyle.Icon}
    */
   buttonStyle?: ButtonStyle;
-
-  /**
-   * Specifics wether the focusin and focusout event propagation should be stopped.
-   *
-   * Default: false
-   */
-  stopPropagationFocus?: boolean;
-
-  /**
-   * Specifics wether the click event propagation should be stopped.
-   *
-   * Default: false
-   */
-  stopPropagationClick?: boolean;
 }
 
 /**
@@ -146,16 +132,7 @@ export class Button<Config extends ButtonConfig> extends Component<Config> {
     // Listen for the click event on the button element and trigger the corresponding event on the button component
     buttonElement.on('click', e => {
       e.preventDefault();
-      if (this.config.stopPropagationClick) {
-        e.stopPropagation();
-      }
       this.onClickEvent();
-    });
-
-    buttonElement.on('focusin focusout', e => {
-      if (this.config.stopPropagationFocus) {
-        e.stopPropagation();
-      }
     });
 
     buttonElement.on('touchstart', e => {
