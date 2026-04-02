1671be0549abd875cef11988f5fe7999c8d3ec30
Daniel Weinberger <dweinber@users.noreply.github.com>
2026-04-02 13:29:45 +0200

    Add JSDoc to defaultLayouts

--- src/ts/UIFactory.ts
+++ src/ts/UIFactory.ts
@@ -218,7 +218,10 @@ export namespace UIFactory {
   }
 
   /**
+   * Default layout functions which are used to build the default UI.
    *
+   * Using these methods enable UI customizations of certains aspects of the default UI, without recreating all UI variants.
+   * It can be used to recreate the default UI but change the coniditions based on which the variant switching happens.
    */
   export namespace defaultLayouts {
     export function subtitleUiLayout(): UIContainer {
