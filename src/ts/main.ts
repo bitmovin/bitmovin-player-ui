8b89b87272c9742738b666cb8139c0e2e247f6d0
Michele Pozzi <123.mpozzi@gmail.com>
2026-04-16 11:38:23 -0600

    Add missing exported types

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -117,8 +117,8 @@ export { WindowColorSelectBox } from './components/settings/subtitlesettings/Win
 export { WindowOpacitySelectBox } from './components/settings/subtitlesettings/WindowOpacitySelectBox';
 export { SubtitleSettingsResetButton } from './components/settings/subtitlesettings/SubtitleSettingsResetButton';
 export { ListBox, ListBoxConfig } from './components/lists/ListBox';
-export { SubtitleListBox } from './components/lists/SubtitleListBox';
-export { AudioTrackListBox } from './components/lists/AudioTrackListBox';
+export { SubtitleListBox, SubtitleListBoxConfig } from './components/lists/SubtitleListBox';
+export { AudioTrackListBox, AudioTrackListBoxConfig } from './components/lists/AudioTrackListBox';
 export { SettingsPanelPage, SettingsPanelPageConfig } from './components/settings/SettingsPanelPage';
 export { SettingsPanelPageBackButton } from './components/settings/SettingsPanelPageBackButton';
 export { SettingsPanelPageOpenButton } from './components/settings/SettingsPanelPageOpenButton';
