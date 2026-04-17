9dad527997ddc782ba21487bff54042433e95347
Michele Pozzi <123.mpozzi@gmail.com>
2026-04-16 18:33:51 -0600

    Ensure spy implementations do not persist into subsequent tests globally

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
 
+  // Restore prototype spy implementations
+  restoreAllMocks: true,
+
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
